80118e8681a9039eedbe9d8e700b33aee9632503
Juancavanagh5 <juancavanagh40@gmail.com>
2017-06-25 22:00:15 -0300

    Add files via upload

---
+++ bin/test/PeliculasModelTest.java
@@ -0,0 +1,53 @@
+package test;
+
+import static org.junit.Assert.*;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import src.Peliculas;
+import src.PeliculasModel;
+import src.Salas;
+import src.Ticket;
+
+
+
+public class PeliculasModelTest {
+	private PeliculasModel modelo=new PeliculasModel();
+	String[] des = {"element1-ok","element2-99","element3-5,5","dsfsdf","adsdd","asas"};
+
+	
+	@Before
+	public void setUp() {
+	modelo.readData();
+	}
+	
+	@Test
+	public void testReadData() {
+		assertEquals(modelo.getAllTheaters().get("Sala A").getLevel(),"normal");				// se quiere probar el tipo de cine disponible, supera el test si se instancia un cine tipo normal.
+		assertFalse(modelo.getAllTheaters().get("Sala A").getLevel()=="Cualquier otra cosa");	// solo pasa si recibe un tipo normal. 
+	}
+
+
+	@Test
+	public void testGetAllMovies() {
+		assertEquals(modelo.getAllMovies().get("Wonder Woman").getTitle(),"Wonder Woman");
+		assertFalse(modelo.getAllMovies().get("Wonder Woman").getTitle()=="Cualquier otro");
+	}
+
+	@Test
+	public void testGetAllTheaters() {
+		assertEquals(modelo.getAllTheaters().containsKey("Sala A"),true);
+		assertFalse(modelo.getAllTheaters().containsKey("Sala A")==false);
+	}
+	
+	@Test
+	public void testGetCount() {
+		
+		assertEquals(modelo.getCount(),1);
+		assertFalse(modelo.getCount()==5);
+	}
+
+}
